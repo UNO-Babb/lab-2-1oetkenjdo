6f277f48e4770753a417609bb4f9159e7af0e383
1oetkenjdo <jacoboetken@unomaha.edu>
2025-02-01 19:08:16 +0000

    Lab 2 Completed

--- FutureTime.py
+++ FutureTime.py
@@ -9,20 +9,24 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  currentHour = (now.hour - 6) % 24 #utc-6 timezone
   currentMinute = now.minute
 
-  print (currentHour, currentMinute) #this is just for checking, we should delete it later
-
-  #TODO:
   #Ask user for hours
-  #Ask user for minutes
+  userinput = input("What time will it be after ____ hours, and ____ minutes? (Enter as 'H MM'): ")
+ 
+  #split answer - hrs and min
+  addHours, addMinutes = map(int, userinput.split())
 
-  #Calculate the time after the user-supplied time has passed.
+  #Calculate the time after the user-supplied time has passed. (NEW TIME)
+  totalMins = currentMinute + addMinutes
+  totalHrs = currentHour + addHours + (totalMins // 60)
 
-  #Do not use any if statements in calculating the time.
+  futureHrs = totalHrs % 24 #(wraps around after 24hrs)
+  futureMin = totalMins % 60 #(wraps around after 60 min)
 
   #Output the future time in standard format "HH:MM"
+  print(f"In {addHours} hours and {addMinutes} minutes, it will be {futureHrs:02d}:{futureMin:02d}.")
 
 
 if __name__ == '__main__':
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,18 +1,27 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name: Jacob Oetken
+#Date: 2/1/2025
+#Assignment: Magic 8 Ball
 
 #We will need random for this program, import to use this package.
 import random
 
 def main():
-  #Create a list of your responses.
-  print("Magic 8 Ball")
-  #Prompt the user for their question.
+    print("Magic 8 Ball")
+    
+    #Prompt the user for their question
+    question = input("Ask a yes/no question: ")
 
-  #Answer question randomly with one of the options from your earlier list.
+    # List of possible answers
+    answers = ["Outlook hazy, look it up on Google.", "Yes, but only if you believe in unicorns.", "Signs point to pizza.", "404 - Future Not Found.", "The stars say maybe, but I'm just a ball.", "In your dreams... literally.", 
+               "Unsure, ask your pet goldfish for advice.", "Without a doubt, if the moon is full.", "My sources say no, but what do they know?", "Don't count on it unless pigs start flying.", "As certain as finding socks in the laundry.", 
+               "Very doubtful, like finding a needle in a haystack.", "Absolutely, just don't ask how.", "Cannot predict now, I'm on a lunch break.", "Sure, why not? Life is a mystery.", "Outlook sunny, but don't forget your umbrella.", 
+               "Better not tell you now, it's a secret.", "You're making a major life decision based on what a plastic ball tells you to do? Jeez...", "Chances are as slim as a spaghetti noodle.", "Absolutely, positively, unequivocally, definitely maybe."]    
+    # Select a random response
+    r = random.randint(0, len(answers) - 1)  # Ensures a valid index
 
+    response = answers[r]
+    print("Magic 8 Ball says:", response)
 
 if __name__ == '__main__':
   main()
